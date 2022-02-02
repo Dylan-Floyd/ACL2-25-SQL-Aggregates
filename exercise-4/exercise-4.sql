81f975f55d1a8afb228b6336838cb6f713d4f27d
Dylan Floyd <floyd800@gmail.com>
2022-02-02 15:59:37 -0800

    i give this lab a 6/10

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,9 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT category.name, AVG(film.length)
+FROM film_category
+LEFT JOIN category
+ON category.category_id = film_category.category_id
+LEFT JOIN film
+ON film.film_id = film_category.film_id
+GROUP BY category.category_id
--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,11 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+SELECT customer.email, RANK() OVER (ORDER BY SUM(film.length) DESC)
+FROM rental
+LEFT JOIN customer
+ON customer.customer_id = rental.customer_id
+LEFT JOIN inventory
+ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN film
+ON film.film_id = inventory.film_id
+GROUP BY customer.email
